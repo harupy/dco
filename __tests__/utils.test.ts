a980b89bfb31da4b330e4f65ce9b7c8d230b3540
harupy <17039389+harupy@users.noreply.github.com>
2020-08-06 22:22:02 +0900

    Fix typo
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,6 +1,6 @@
 import { createCodeBlock } from '../src/utils';
 
-describe('utls', () => {
+describe('utils', () => {
   it(createCodeBlock.name, () => {
     expect(createCodeBlock('echo foo')).toBe('\n\necho foo\n\n');
   });
